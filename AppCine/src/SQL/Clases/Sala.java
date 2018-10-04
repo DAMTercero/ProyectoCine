05efeae3fe4c8b15d7b863ac6f3355defa4487be
Joseba <josepo2000@gmail.com>
2018-10-04 21:04:39 +0200

    clases y sql

--- AppCine/src/SQL/Clases/Sala.java
+++ AppCine/src/SQL/Clases/Sala.java
@@ -11,4 +11,89 @@ package SQL.Clases;
  */
 public class Sala {
     
+    private static final String mysqlConector = "mysql";
+    private String conector;
+
+    private int ID_SALA;
+    private int CAPACIDAD;
+    private int PANTALLA;
+    private String APERTURA;
+    private String Horario;
+    private boolean DISPONIBLE;
+   
+
+    public Sala() {
+    }
+
+    public Sala(int ID_SALA, int CAPACIDAD, int PANTALLA, String APERTURA, String Horario, boolean DISPONIBLE) {
+        this.ID_SALA = ID_SALA;
+        this.CAPACIDAD = CAPACIDAD;
+        this.PANTALLA = PANTALLA;
+        this.APERTURA = APERTURA;
+        this.Horario = Horario;
+        this.DISPONIBLE = DISPONIBLE;
+        
+    }
+    
+
+    
+    
+    
+    
+    public String getConector() {
+        return conector;
+    }
+
+    public void setConector(String conector) {
+        this.conector = conector;
+    }
+
+    public int getID_SALA() {
+        return ID_SALA;
+    }
+
+    public void setID_SALA(int ID_SALA) {
+        this.ID_SALA = ID_SALA;
+    }
+
+    public int getCAPACIDAD() {
+        return CAPACIDAD;
+    }
+
+    public void setCAPACIDAD(int CAPACIDAD) {
+        this.CAPACIDAD = CAPACIDAD;
+    }
+
+    public int getPANTALLA() {
+        return PANTALLA;
+    }
+
+    public void setPANTALLA(int PANTALLA) {
+        this.PANTALLA = PANTALLA;
+    }
+
+    public String getAPERTURA() {
+        return APERTURA;
+    }
+
+    public void setAPERTURA(String APERTURA) {
+        this.APERTURA = APERTURA;
+    }
+
+    public String getHorario() {
+        return Horario;
+    }
+
+    public void setHorario(String Horario) {
+        this.Horario = Horario;
+    }
+
+    public boolean getDISPONIBLE() {
+        return DISPONIBLE;
+    }
+
+    public void setDISPONIBLE(boolean DISPONIBLE) {
+        this.DISPONIBLE = DISPONIBLE;
+    }
+
 }
