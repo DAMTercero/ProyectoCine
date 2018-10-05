35426f74f93ffd262740eb7f627066c4cf0632f9
Joseba <josepo2000@gmail.com>
2018-10-05 21:27:52 +0200

    metodos

--- AppCine/src/SQL/Dao/SqlCrude.java
+++ AppCine/src/SQL/Dao/SqlCrude.java
@@ -11,6 +11,7 @@ import SQL.Clases.Sala;
 import SQL.Conexion.sql;
 import java.sql.Connection;
 import static java.sql.DriverManager.getConnection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -116,6 +117,10 @@ public class SqlCrude extends SQL.Conexion.sql{
             
         } 
     
+    
+    
+    
+    
     private static Object parseObject(ResultSet rs, String tipo) throws SQLException {
         if (tipo.equals(Sala.class.getSimpleName())) {
             //Creamos el objeto sala 
@@ -133,6 +138,273 @@ public class SqlCrude extends SQL.Conexion.sql{
             return null;
         }
     }
+    
+    
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    
+     public static Sala getSalaById(int Id_Sala, String conector) throws SQLException, SQLException, SQLException, SQLException {
+        //Objeto jugador
+        Sala obj = null;
+        Connection conn = null;
+
+        try {
+            //Obtenemos el conector
+            conn = getConnection(conector);
+
+            //Query
+            String query = "SELECT * FROM SALA WHERE ID_SALA = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            //Añadimos el id a la condicion
+            ps.setInt(1, Id_Sala);
+
+            //Ejecutamos la query
+            ResultSet rs = ps.executeQuery();
+
+            //Recorremos el resultado
+            while (rs.next()) {
+                //Obtenemos la sala
+                obj = (Sala) parseObject(rs, Sala.class.getSimpleName());
+
+                //Comprobamos que no sea nulo
+                if (obj != null) {
+                    //Añadimos el conector
+                    obj.setConector(conector);
+
+                   
+                }
+            }
+
+            //Cerramos conexion
+            conn.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Imposible establecer conexion con las bases de datos.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+        //Devolvemos la sala
+        return obj;
+    }
+    
+    
+    /////////////////////////////////////////////////////////////////////
+     
+    public static Pelicula getPeliculaById(int Id_Pelicula, String conector) throws SQLException, SQLException, SQLException, SQLException {
+        //Objeto jugador
+        Pelicula obj = null;
+        Connection conn = null;
+
+        try {
+            //Obtenemos el conector
+            conn = getConnection(conector);
+
+            //Query
+            String query = "SELECT * FROM PELICULA WHERE ID_PELICULA = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            //Añadimos el id a la condicion
+            ps.setInt(1, Id_Pelicula);
+
+            //Ejecutamos la query
+            ResultSet rs = ps.executeQuery();
+
+            //Recorremos el resultado
+            while (rs.next()) {
+                //Obtenemos la sala
+                obj = (Pelicula) parseObject(rs, Pelicula.class.getSimpleName());
+
+                //Comprobamos que no sea nulo
+                if (obj != null) {
+                    //Añadimos el conector
+                    obj.setConector(conector);
+
+                   
+                }
+            }
+
+            //Cerramos conexion
+            conn.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Imposible establecer conexion con las bases de datos.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+        //Devolvemos la sala
+        return obj;
+    } 
+    
+    
+    ///////////////////////////////////////////////////////////////////////
+      
+    public static Pelicula getPeliculaByTitle(int TITULO, String conector) throws SQLException, SQLException, SQLException, SQLException {
+        //Objeto jugador
+        Pelicula obj = null;
+        Connection conn = null;
+
+        try {
+            //Obtenemos el conector
+            conn = getConnection(conector);
+
+            //Query
+            String query = "SELECT * FROM PELICULA WHERE TITULO = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            //Añadimos el id a la condicion
+            ps.setInt(1, TITULO);
+
+            //Ejecutamos la query
+            ResultSet rs = ps.executeQuery();
+
+            //Recorremos el resultado
+            while (rs.next()) {
+                //Obtenemos la sala
+                obj = (Pelicula) parseObject(rs, Pelicula.class.getSimpleName());
+
+                //Comprobamos que no sea nulo
+                if (obj != null) {
+                    //Añadimos el conector
+                    obj.setConector(conector);
+
+                   
+                }
+            }
+
+            //Cerramos conexion
+            conn.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Imposible establecer conexion con las bases de datos.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+        //Devolvemos la sala
+        return obj;
+    } 
+    
+    //////////////////////////////////////////////////////////7
+    
+      
+    public static Pelicula getPeliculaByANYO_STRENO(int ANYO_STRENO, String conector) throws SQLException, SQLException, SQLException, SQLException {
+        //Objeto jugador
+        Pelicula obj = null;
+        Connection conn = null;
+
+        try {
+            //Obtenemos el conector
+            conn = getConnection(conector);
+
+            //Query
+            String query = "SELECT * FROM PELICULA WHERE ANYO_STRENO = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            //Añadimos el id a la condicion
+            ps.setInt(1, ANYO_STRENO);
+
+            //Ejecutamos la query
+            ResultSet rs = ps.executeQuery();
+
+            //Recorremos el resultado
+            while (rs.next()) {
+                //Obtenemos la sala
+                obj = (Pelicula) parseObject(rs, Pelicula.class.getSimpleName());
+
+                //Comprobamos que no sea nulo
+                if (obj != null) {
+                    //Añadimos el conector
+                    obj.setConector(conector);
+
+                   
+                }
+            }
+
+            //Cerramos conexion
+            conn.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Imposible establecer conexion con las bases de datos.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+        //Devolvemos la sala
+        return obj;
+    } 
+    /////////////////////////////////////////////
+    
+    public static Pelicula getPeliculaByDIRECTOR(int DIRECTOR, String conector) throws SQLException, SQLException, SQLException, SQLException {
+        //Objeto jugador
+        Pelicula obj = null;
+        Connection conn = null;
+
+        try {
+            //Obtenemos el conector
+            conn = getConnection(conector);
+
+            //Query
+            String query = "SELECT * FROM PELICULA WHERE DIRECTOR = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            //Añadimos el id a la condicion puede que sea el error
+            ps.setInt(1, DIRECTOR);
+
+            //Ejecutamos la query
+            ResultSet rs = ps.executeQuery();
+
+            //Recorremos el resultado
+            while (rs.next()) {
+                //Obtenemos la sala
+                obj = (Pelicula) parseObject(rs, Pelicula.class.getSimpleName());
+
+                //Comprobamos que no sea nulo
+                if (obj != null) {
+                    //Añadimos el conector
+                    obj.setConector(conector);
+
+                   
+                }
+            }
+
+            //Cerramos conexion
+            conn.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Imposible establecer conexion con las bases de datos.", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+        //Devolvemos la sala
+        return obj;
+    }
+    
+    
+    
     
     
     
